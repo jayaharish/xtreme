42aea33526a85b8dbba8a833cfb6d066b791a50e
jayaharish <61152197+jayaharish@users.noreply.github.com>
2020-04-16 18:42:17 +0530

    initial commit

---
+++ build/precache-manifest.e124743883f1d2d1341a3932c2e66ff5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df3ba34ee3d68c1a45126649ede285c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a1e159916a5e8aeec4e",
+    "url": "/static/css/main.e14413eb.chunk.css"
+  },
+  {
+    "revision": "6a0c44ce8d7d06b9025a",
+    "url": "/static/js/2.0467b3c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0467b3c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1e159916a5e8aeec4e",
+    "url": "/static/js/main.9669dbde.chunk.js"
+  },
+  {
+    "revision": "245673e8f524891bcb9a",
+    "url": "/static/js/runtime-main.3123f9bb.js"
+  },
+  {
+    "revision": "fa82e8d491ed6acb07fd39acc11e5dbe",
+    "url": "/static/media/one.fa82e8d4.jpg"
+  },
+  {
+    "revision": "56e7ab34b2638d01b9a6dc0363637d83",
+    "url": "/static/media/skater.56e7ab34.png"
+  },
+  {
+    "revision": "720e8cc85a5024c8449942780be0dc79",
+    "url": "/static/media/third.720e8cc8.jpg"
+  },
+  {
+    "revision": "facd9d96cd3cc1ce6fa4585a05846246",
+    "url": "/static/media/two.facd9d96.jpg"
+  }
+]);
